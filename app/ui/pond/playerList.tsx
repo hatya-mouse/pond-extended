7def6c4eb058ea8634cafe4ff62a9e9f3f141040
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-05-27 22:37:58 +0900

    fix: add whitespace-nowrap to ducks' name Fix the problem that line wraps when the duck's health is low

--- app/ui/pond/playerList.tsx
+++ app/ui/pond/playerList.tsx
@@ -15,7 +15,7 @@
 //
 
 // Module Import
-import clsx from "clsx"
+import clsx from "clsx";
 // Pond Game Imports
 import Duck from "@app/types/duck";
 import { PondSettings } from "@app/types/pond.types";
@@ -23,19 +23,19 @@ import { PondSettings } from "@app/types/pond.types";
 export default function PlayerList({
     ducks,
     latestSettings,
-    onSelectDuck = () => { },
+    onSelectDuck = () => {},
 }: {
-    ducks: Duck[],
+    ducks: Duck[];
     /**
      * Latest settings used to check the diff between
      * current the in-game ducks and the latest settings ducks.
      */
-    latestSettings: PondSettings,
+    latestSettings: PondSettings;
     /**
      * Called when the duck is selected.
      * @param {number} _ ID of the selected duck, not index.
      */
-    onSelectDuck?: (_: number) => void,
+    onSelectDuck?: (_: number) => void;
 }) {
     return (
         <div className="float-container">
@@ -54,20 +54,23 @@ export default function PlayerList({
                         className="overflow-visible"
                         style={{
                             backgroundColor: duck.colour,
-                            width: `${(100 - duck.damage)}%`,
+                            width: `${100 - duck.damage}%`,
                         }}
                     >
-                        <div className={clsx(
-                            "p-1 px-2",
-                            "player-list-item-text",
-                            duck.dead ? "text-red-400" : "text-white"
-                        )}>
-                            {/* Show duck's name. */}
+                        <div
+                            className={clsx(
+                                "p-1 px-2 whitespce-nowrap",
+                                "player-list-item-text",
+                                duck.dead ? "text-red-400" : "text-white",
+                            )}
+                        >
+                            {/* Show duck's name */}
                             {duck.name}
-                            {
-                                latestSettings.ducks.filter((data) => data.id === duck.id).length === 0 &&
+                            {latestSettings.ducks.filter(
+                                (data) => data.id === duck.id,
+                            ).length === 0 && (
                                 <div className="text-gray-200">Removed</div>
-                            }
+                            )}
                         </div>
                     </div>
                 </div>
